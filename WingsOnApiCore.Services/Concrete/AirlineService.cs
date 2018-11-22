ce43dcf22073a2e282d1991c5b66f3819853bb2c
Randy Rodriguez <randyro91@hotmail.com>
2018-11-22 16:05:05 +0100

    Implement basic functionality of AirlineService

--- WingsOnApiCore.Services/Concrete/AirlineService.cs
+++ WingsOnApiCore.Services/Concrete/AirlineService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using WingsOnApiCore.Models;
 using WingsOnApiCore.Services.Abstract;
@@ -8,22 +9,27 @@ namespace WingsOnApiCore.Services.Concrete
     {
         public AirlineModel Get(int id)
         {
-            throw new System.NotImplementedException();
+            if (id < 0)
+            {
+                throw new ArgumentException($"The Id cannot be negative: {id}");
+            }
+            
+            return DbContext.AirlineRepository.Get(id);
         }
 
         public IEnumerable<AirlineModel> GetAll()
         {
-            throw new System.NotImplementedException();
+            return DbContext.AirlineRepository.GetAll();
         }
 
         public AirlineModel GetAirlineByName(string airlineName)
         {
-            throw new System.NotImplementedException();
+            return DbContext.AirlineRepository.GetAirlineByName(airlineName);
         }
 
         public AirlineModel GetAirlineByCode(string airlineCode)
         {
-            throw new System.NotImplementedException();
+            return DbContext.AirlineRepository.GetAirlineByCode(airlineCode);
         }
     }
 }
